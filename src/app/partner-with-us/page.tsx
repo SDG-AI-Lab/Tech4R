5213ffc6c0f2551c87bef2727d9871aea539069a
Shallow Assumptions <mudawiabubaker@gmail.com>
2025-06-19 21:15:04 +0100

    using tailwind classes + Title Component

--- src/app/partner-with-us/page.tsx
+++ src/app/partner-with-us/page.tsx
@@ -1,8 +1,8 @@
 import { Hero } from "@/components/Hero";
 import { Button } from '@/components/Button'
+import Title from '@/components/Title'
 import Image from "next/image";
 import debugimg from "@/app/volunteers/debugimg.png"
-import styles from "@/app/partner-with-us/partner.module.css"
 
 //Dummy data; todo: pull from actual database or hardcode it in
 const partners = Array(6).fill({name:"Partner X",desc:"Lorem Ipsum",logo:debugimg});
@@ -22,7 +22,7 @@ export default function PartnerWithUsPage() {
           <Image src={debugimg} alt="ALT TEXT" height={275} className="flex-1 rounded-3xl"/>
           
           <div className="container mx-auto flex-1 p-12 m-6">
-            <h2 className={styles.title}>Why Partner with Tech4R?</h2>
+            <Title>Why Partner with Tech4R?</Title>
             <p className="text-neutral-03 p-4 mb-4">Lorem ipsum etc etc etc etc<br />Description goes here</p>
             <ul className="grid gap-4 grid-cols-2 place-items-center place-content-center text-neutral-03 p-4 mb-4">
               {reasons.map(e => (<li key={e} className="w-full">{e}</li>))}
@@ -32,7 +32,7 @@ export default function PartnerWithUsPage() {
           
         </div>  
         <div className="container mx-auto p-4 m-6 bg-neutral-01 rounded-3xl">
-          <h2 className={styles.title}>Host Organisations & Partners</h2>
+          <Title>Host Organisations & Partners</Title>
           <ul className="grid gap-4 grid-cols-2 md:grid-cols-3 place-items-center place-content-center">
             {partners.map(e => (
               <li key={e.name} className="rounded-lg p-4 w-full bg-background m-4">
--- src/app/volunteers/page.tsx
+++ src/app/volunteers/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from '@/components/Hero'
-import {Button } from '@/components/Button'
+import { Button } from '@/components/Button'
+import Title from '@/components/Title'
 import Image from "next/image";
 import styles from "@/app/volunteers/volunteer.module.css"
 import debugimg from "@/app/volunteers/debugimg.png"
@@ -22,12 +23,12 @@ export default function VolunteersPage() {
         <div className="container mx-auto p-4 m-6">
           <Carousel pages={1+Math.floor(0.125 * (volunteers.length-1))} size={8} className="grid gap-4 grid-cols-2 md:grid-cols-4 place-items-center place-content-center">
             {volunteers.map((e,i) => (
-              <CI key={e.name} className={`rounded-lg w-full ${styles.volunteer}`} index={i}>
-                <Image alt={e.name} src={e.src}/>
-                <div className={styles.overlay}>
-                  <div className='p-4'>
-                    <h5>{e.name}{i}</h5><p className='text-neutral-02'>{e.loc}</p>
-                    {/*todo: LinkedIn*/}
+              <CI key={e.name} className="group rounded-lg w-full contain-content z-1 m-3" index={i}>
+                <Image alt={e.name} src={e.src} className='object-cover w-full z-2'/>
+                <div className={`${styles.overlay} fixed bottom-0 w-full h-6/12 z-3 opacity-0 group-hover:opacity-100 text-white duration-200 bg-gradient-to-t from-black bg-opacity-75`}>
+                  <div className='fixed bottom-0 w-full p-4 z-4'>
+                    <h5 className='text-2xl font-bold'>{e.name}{i}</h5><p className='text-neutral-01 font-light text-xs leading-relaxed'>{e.loc}</p>
+                    {/*todo: LinkedIn icon*/}
                   </div>
                 </div>
               </CI>
@@ -35,26 +36,26 @@ export default function VolunteersPage() {
           </Carousel>
         </div>
 
-        <div id={styles.skills} className="container mx-auto p-4 m-6">
-          <h2 className={styles.title}>Required Skills</h2>
+        <div className="container mx-auto p-4 m-6 bg-neutral-01 rounded-3xl">
+          <Title> Required Skills </Title>
           <ul className="grid gap-4 grid-cols-2 md:grid-cols-3 place-items-center place-content-center">
             {skills.map(i => (
-              <li key={i.name} className={`rounded-lg p-4 w-full ${styles.skill}`}>
-                {/*<Image src="" alt="vName"/>*/}
-                <h5>{i.name}</h5>
-                <p className='text-neutral-02'>{i.desc}</p>
+              <li key={i.name} className="rounded-lg p-4 w-full m-3 contain-content bg-background">
+                {/*todo: Icons*/}
+                <h5 className='text-2xl font-bold'>{i.name}</h5>
+                <p className='text-neutral-02 font-light leading-relaxed'>{i.desc}</p>
               </li>
             ))}
           </ul>
         </div>
 
         <div className="container mx-auto p-4 m-6">
-          <h2 className={styles.title}>How to Get Involved as a Volunteer</h2>
+          <Title>How to Get Involved as a Volunteer</Title>
           <ul className={`grid gap-4 grid-cols-2 md:grid-cols-${steps.length} place-items-center place-content-center`}>
             {steps.map((e,i) => (
-              <li key={`step${i}`} className={`rounded-lg p-4 w-full ${styles.step}`}>
-                {/*<Image src="" alt="vName"/>*/}
-                <h5>{e.name}</h5>
+              <li key={`step${i}`} className="rounded-lg p-4 w-full m-3 contain-content text-center bg-background border-1 border-neutral-500 border-opacity-20">
+                {/*todo: Icons*/}
+                <h5 className='text-2xl font-bold'>{e.name}</h5>
                 <p className='text-neutral-03'>{e.desc}</p>
               </li>
             ))}
@@ -62,17 +63,17 @@ export default function VolunteersPage() {
         </div>
 
         <div className="container mx-auto p-4 m-6">
-          <h2 className={styles.title}>Volunteer Testimonials</h2>
+          <Title>Volunteer Testimonials</Title>
           <Carousel pages={1+Math.floor((testimony.length-1)/6)} size={6} className="grid gap-4 grid-cols-2 md:grid-cols-3 place-items-center place-content-center">
             {testimony.map((e,i) => (
-              <CI key={e.title} className={`rounded-lg w-full p-4 ${styles.testimony}`} index={i}>
-                {/*todo: Quote*/}
-                <h5>{e.title}{i}</h5>
+              <CI key={e.title} className="rounded-lg w-full p-4 m-3 contain-content bg-neutral-01" index={i}>
+                {/*todo: Quote icon*/}
+                <h5 className='text-2xl font-bold'>{e.title}{i}</h5>
                 <p className='text-neutral-03'>{e.test}</p>
-                <div className="flex mt-4 origin">
-                  <Image alt={e.vname} src={e.vsrc} height={48} className='mr-4'/>
+                <div className="flex mt-4">
+                  <Image alt={e.vname} src={e.vsrc} height={48} className='mr-4 rounded-full'/>
                   <div>
-                    <h6>{e.vname}</h6>
+                    <h6 className='text-lg font-bold'>{e.vname}</h6>
                     <p className='text-neutral-03'>{e.vtitle}</p>
                   </div>
                 </div>
@@ -82,12 +83,12 @@ export default function VolunteersPage() {
         </div>
 
         <div className="container grid items-center mx-auto p-4 m-6">
-          <h2 className={styles.title}>Browse Volunteer Assignments in Ongoing Projects</h2>
+          <Title>Browse Volunteer Assignments in Ongoing Projects</Title>
           <Button className="m-auto flex-none" href=""><span className="hidden md:inline">Become a&nbsp;</span>Volunteer</Button>
           <Carousel pages={1+Math.floor((assignments.length-1)/6)} size={6} className="grid gap-4 grid-cols-2 md:grid-cols-3 place-items-center place-content-center">
             {assignments.map((e,i) => (
-              <CI key={e.title} className={`rounded-lg bg-color-02 text-neutral-02 w-full p-4 ${styles.assignment}`} index={i}>
-                <h5>{e.title}{i}</h5>
+              <CI key={e.title} className="rounded-lg bg-color-02 text-neutral-02 w-full p-4 m-3 contain-content" index={i}>
+                <h5 className='text-white text-2xl font-bold'>{e.title}{i}</h5>
                 <p>{e.desc}</p>
                 <p>{e.meta}</p>
                 <Button className="mt-4 flex-none" variant='white' href={e.href}>Apply<span className="hidden md:inline">&nbsp;Now</span></Button>
