ce7690f6d8ff14ae3a3990fbdb366de5a4390b84
Jake <jakepyne@gmail.com>
2025-09-21 02:27:24 +0200

    Rm indices

--- src/app/volunteers/CarouselAssignments.tsx
+++ src/app/volunteers/CarouselAssignments.tsx
@@ -12,8 +12,8 @@ async function AssignmentsCarousel() {
   }/**/
   return (
     <Carousel pages={1 + Math.floor((assignments.length - 1) / 6)} size={6} className="grid gap-4 grid-cols-1 md:grid-cols-3 place-items-center place-content-center">
-      {assignments.map((e, i) => (
-        <CI key={e.title} className="rounded-2xl bg-color-02 text-neutral-02 w-full h-full p-4 m-3 grid contain-content" index={i}>
+      {assignments.map((e) => (
+        <CI key={e.title} className="rounded-2xl bg-color-02 text-neutral-02 w-full h-full p-4 m-3 grid contain-content">
           <h5 className='text-white text-2xl font-bold'>{e.title}</h5>
           <p>{e.description}</p>
           <div className='bottom-0 self-end'>
